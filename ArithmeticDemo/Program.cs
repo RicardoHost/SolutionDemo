dfe73c2b3407b88c5dd8d26ebb0166cd1cebb0e4
RicardoHost <1990354025@qq.com>
2025-01-17 10:26:37 +0800

    Create ArithmeticDemo

---
+++ ArithmeticDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArithmeticDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
