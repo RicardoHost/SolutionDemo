d9f56394cb6556cf6896f0265bd96349ed52df36
RicardoHost <1990354025@qq.com>
2025-01-17 10:42:20 +0800

    add ArithmeticDemo/Sort

---
+++ ArithmeticDemo/Sort/BubblingSort.cs
@@ -0,0 +1,18 @@
+﻿using ArithmeticDemo.Sort.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArithmeticDemo.Sort
+{
+    /// <summary>
+    /// 冒泡排序
+    /// </summary>
+    public class BubblingSort : ISort
+    {
+        public int[] Sort(int[] arr)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ArithmeticDemo/Sort/interfaces/ISort.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArithmeticDemo.interfaces
+{
+    public interface ISort
+    {
+        int[] Sort(int[] arr);
+    }
+}
