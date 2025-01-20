b33fd8fae58bbaeb89bb109a0472bd743213f3e4
RicardoHost <1990354025@qq.com>
2025-01-20 11:26:06 +0800

    rename Demo.Main

---
+++ Demo.Main/Program.cs
@@ -0,0 +1,31 @@
+﻿using Demo.Arithmetic.Sort;
+using Demo.Arithmetic.Sort.interfaces;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sort();
+        }
+
+        public static void Sort()
+        {
+            var random = new Random();
+            var arr = Enumerable.Range(1, 10000).Select(it => random.Next(10000)).ToArray();
+            ISort sort = new InsertionSort();
+
+            var start = DateTime.Now.Ticks;
+            arr = sort.sort(arr);
+            var end = DateTime.Now.Ticks;
+            Console.WriteLine(end - start);
+            Console.WriteLine(string.Join(",", arr));
+        }
+    }
+}
